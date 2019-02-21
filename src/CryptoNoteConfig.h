f33f6f6ff44bd836d623686d13cf1d39b083cd25
OCKHAM <ceo@ockham.consulting>
2019-02-21 20:00:10 +0100

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -27,7 +27,7 @@ namespace parameters {
 const uint32_t CRYPTONOTE_MAX_BLOCK_NUMBER                   = 500000000;
 const size_t   CRYPTONOTE_MAX_BLOCK_BLOB_SIZE                = 500000000;
 const size_t   CRYPTONOTE_MAX_TX_SIZE                        = 1000000000;
-const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 6; // addresses start with "2"
+const uint64_t CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX       = 0x39d362; // addresses start with prefix "euor"
 const uint32_t CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW          = 10;
 const uint64_t CRYPTONOTE_BLOCK_FUTURE_TIME_LIMIT            = 60 * 60 * 2;
   
@@ -50,11 +50,11 @@ const size_t   CRYPTONOTE_DISPLAY_DECIMAL_POINT              = 8;
 const uint64_t MINIMUM_FEE                                   = UINT64_C(1000000);    // pow(10, 6)
 const uint64_t DEFAULT_DUST_THRESHOLD                        = UINT64_C(1000000);    // pow(10, 6)
 
-const size_t   ADDRESSES_STAKED_PER_BLOCK                    = 1;  // How many addresses get a staking reward per block
-const size_t   STAKING_CHANCE_PER_BLOCK                      = 0.1 // Basically the chance that you will get a staking reward per block, every time you dont get a reward, your staking chance increases by the amount you set it as. When you get your staking reward, your staking chance resets to 0.
-const size_t   SECONDS_BEFORE_COIN_MATURITY                  = 10000000; // Basically how many seconds before staking begins, set it seconds so it can be exact
-const uint64_t MINIMUM_STAKE_AMOUNT                          = UINT64_C(10000000); // This is how many coins you need for staking, make sure to put the amount needed in atomic units!!! p.s, if your coins maximum supply is via uint64_t -1 replace UINT64_C with that
-const size_t   STAKE_PERCENT_PER_YEAR                        = 5;   // Basically how much the stake reward is per year in percent
+const size_t   ADDRESSES_STAKED_PER_BLOCK                    = 1;                   // How many addresses get a staking reward per block
+const size_t   STAKING_CHANCE_PER_BLOCK                      = 0.1                  // Basically the chance that you will get a staking reward per block, every time you dont get a reward, your staking chance increases by the amount you set it as. When you get your staking reward, your staking chance resets to 0.
+const size_t   SECONDS_BEFORE_COIN_MATURITY                  = 10000000;            // Basically how many seconds before staking begins, set it seconds so it can be exact
+const uint64_t MINIMUM_STAKE_AMOUNT                          = UINT64_C(10000000);  // This is how many coins you need for staking, make sure to put the amount needed in atomic units!!! p.s, if your coins maximum supply is via uint64_t -1 replace UINT64_C with that
+const size_t   STAKE_PERCENT_PER_YEAR                        = 5;                   // Basically how much the stake reward is per year in percent
   
 const uint64_t DIFFICULTY_TARGET                             = 120; // seconds
 const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 / DIFFICULTY_TARGET;
@@ -87,15 +87,16 @@ const uint32_t UPGRADE_WINDOW                                = EXPECTED_NUMBER_O
 static_assert(0 < UPGRADE_VOTING_THRESHOLD && UPGRADE_VOTING_THRESHOLD <= 100, "Bad UPGRADE_VOTING_THRESHOLD");
 static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 
-const char     CRYPTONOTE_BLOCKS_FILENAME[]                  = "blocks.bin";
-const char     CRYPTONOTE_BLOCKINDEXES_FILENAME[]            = "blockindexes.bin";
-const char     CRYPTONOTE_POOLDATA_FILENAME[]                = "poolstate.bin";
-const char     P2P_NET_DATA_FILENAME[]                       = "p2pstate.bin";
-const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json";
+const char     CRYPTONOTE_BLOCKS_FILENAME[]                  = "laroue-blocks.bin";
+const char     CRYPTONOTE_BLOCKINDEXES_FILENAME[]            = "laroue-blockindexes.bin";
+const char     CRYPTONOTE_POOLDATA_FILENAME[]                = "laroue-poolstate.bin";
+const char     P2P_NET_DATA_FILENAME[]                       = "laroue-p2pstate.bin";
+const char     MINER_CONFIG_FILE_NAME[]                      = "laroue-miner_conf.json";
 } // parameters
 
 // Change the name of your coin lol
-const char     CRYPTONOTE_NAME[]                             = "Cryptostake";
+const char     CRYPTONOTE_NAME[]                             = "laroue";
+
 // Keep in mind unlike Cryptonote reference code, the genesis block tx is in src/CryptoNoteCore/currency.cpp, so put your genesis block there
 const uint8_t  TRANSACTION_VERSION_1                         =  1;
 const uint8_t  TRANSACTION_VERSION_2                         =  2;
@@ -110,8 +111,8 @@ const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  //by def
 const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  100;    //by default, blocks count in blocks downloading
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
-const int      P2P_DEFAULT_PORT                              =  8080; // P2P blockchain port
-const int      RPC_DEFAULT_PORT                              =  8081; // JSON RPC port
+const int      P2P_DEFAULT_PORT                              =  37236; // P2P blockchain port
+const int      RPC_DEFAULT_PORT                              =  38236; // JSON RPC port
 
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
@@ -129,8 +130,8 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[] = "E29507CA55455F37A3B783EE2C5123B8B6A34A0C5CAAE050922C6254161480C1"; // I honestly dont know what this is lol
 
 const char* const SEED_NODES[] = {
-  "seed.bytecoin.org:8080", // CHANGE YOUR FUCKIN SEED NODES
-  "85.25.201.95:8080", // CHANGE YOUR FUCKIN SEED NODES
+  "192.168.0.14:37236", // CHANGE YOUR FUCKIN SEED NODES
+  "192.168.0.19:38236", // CHANGE YOUR FUCKIN SEED NODES
 };
 
 struct CheckpointData {
